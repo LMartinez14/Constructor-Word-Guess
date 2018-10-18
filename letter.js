ab326f535b2fce608bcba7bf0325d172307409d7
Laurence <laurence.martinez14@gmail.com>
2018-10-18 18:07:41 -0500

    started word file

---
+++ letter.js
@@ -0,0 +1,14 @@
+// create the letter constructor
+function Letter (letter){
+   this.letter = letter;
+   this.space = "_";
+   this.status = false;
+}
+
+
+
+
+
+
+// make this file exportable
+module.exports = letter;
