f36f49772715842a9eaaeb5b071cabc9f207aef9
Laurence <laurence.martinez14@gmail.com>
2018-10-18 18:24:21 -0500

    test

--- letter.js
+++ letter.js
@@ -1,11 +1,19 @@
 // create the letter constructor
 function Letter (letter){
+   this.status = false;
    this.letter = letter;
    this.space = "_";
-   this.status = false;
 }
 
-
+// create the function to check a guess
+checkInput (){
+   if (this.status){
+      return this.letter;
+   }
+   else (){
+      return this.space;
+   }
+}
 
 
 
